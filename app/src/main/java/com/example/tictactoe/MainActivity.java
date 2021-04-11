f12b9f6c22a16d8333699e9cb54b05cd64355666
ASUS <mahmut.kbn@outlook.com.tr>
2021-04-11 23:24:16 +0300

    Tic-Tac-Toe Application

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void playButtonClicked(View view) {
+        Intent intent = new Intent(this,PlayerSetups.class);
+        startActivity(intent);
+
+    }
+
+}
